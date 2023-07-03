872e6f1a742ed7e63054065bfe87b264a56831ba
lynxnb <niccolo.betto@gmail.com>
2023-07-04 01:45:07 +0200

    Rework script to support updating submodules

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Usage: ./build.sh [path]
-
-if [ -z "$1" ]
-then
-    GIT="git"
-else
-    GIT="git -C $1"
-fi
-
 declare -a modules=(
     "array"
     "assert"
@@ -52,42 +43,102 @@ declare -a modules=(
     "winapi"
 )
 
-for i in "${modules[@]}"
+print_help() {
+    echo "Usage: $0 [-s] PATH"
+    echo "Options:"
+    echo "  -s      Skip updating existing submodules, but only add new ones"
+    echo "Arguments:"
+    echo "  PATH    Path to the working repo"
+}
+
+skip_update=false
+
+# Parse command line arguments
+while getopts hs option
 do
-    $GIT submodule add --name $i --depth 1 https://github.com/boostorg/$i.git libs/$i
-    $GIT config -f .gitmodules submodule.$i.shallow true
-    $GIT add .
-    $GIT commit -m "Add \`$i\` submodule"
+    case "$option"
+    in
+        s) skip_update=true;;
+        h) print_help; exit 0;;
+    esac
 done
+shift $(($OPTIND-1))
+path="$1"
 
-# Add cmake submodule in the tools folder
-$GIT submodule add --name cmake --depth 1 https://github.com/boostorg/cmake.git tools/cmake
-$GIT config -f .gitmodules submodule.cmake.shallow true
-$GIT add .
-
-# Prefix $1 to CMakeLists.txt if it's not empty
-if [ -z "$1" ]
+# cd into the specified path
+if [ -n "$path" ]
 then
-    CMAKE_OUT_FILE="CMakeLists.txt"
-else
-    CMAKE_OUT_FILE="$1/CMakeLists.txt"
+    cd $path &> /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "Error: $path is not a valid path"
+        exit 1
+    fi
 fi
 
-# Fetch CMakeLists.txt from the boostorg/boost repo
-wget https://raw.githubusercontent.com/boostorg/boost/master/CMakeLists.txt -O $CMAKE_OUT_FILE
+# $1 - submodule name
+# $2 - submodule path
+add_submodule() {
+    echo "Adding submodule $1"
+    git submodule add --name $1 --depth 1 https://github.com/boostorg/$1.git $2/$1
+    git config -f .gitmodules submodule.$1.shallow true
+    git add .
+    git commit -m "Add \`$1\` submodule"
+}
 
-# Path CMakeLists.txt to define a Boost::boost target as a sum of all modules
-echo "" >> $CMAKE_OUT_FILE
-echo "add_library(boost INTERFACE)" >> $CMAKE_OUT_FILE
-echo "target_link_libraries(boost INTERFACE" >> $CMAKE_OUT_FILE
+# $1 - submodule name
+# $2 - submodule path
+update_submodule() {
+    if [ $skip_update = true ]
+    then
+        echo "Skipping submodule $1 update"
+        return
+    fi
+
+    echo "Updating submodule $1"
+    cd $2/$1
+    git fetch --depth 1
+    git checkout origin/HEAD
+    cd -
+    git add $2/$1
+    git commit -m "Update \`$1\` submodule"
+}
+
+# $1 - submodule name
+# $2 - submodule path
+add_or_update_submodule() {
+    git submodule status $2/$1
+    if [ $? -eq 0 ]
+    then
+        update_submodule $1 $2
+    else
+        add_submodule $1 $2
+    fi
+}
+
 for i in "${modules[@]}"
 do
-    echo "    Boost::$i" >> $CMAKE_OUT_FILE
+    add_or_update_submodule $i libs
 done
-echo ")" >> $CMAKE_OUT_FILE
-echo "add_library(Boost::boost ALIAS boost)" >> $CMAKE_OUT_FILE
-echo "" >> $CMAKE_OUT_FILE
+
+# Add/Update cmake submodule in the tools folder
+add_or_update_submodule cmake tools
+
+# Fetch CMakeLists.txt from the boostorg/boost repo
+wget https://raw.githubusercontent.com/boostorg/boost/master/CMakeLists.txt -O CMakeLists.txt
+
+# Patch CMakeLists.txt to define a Boost::boost target as a sum of all modules
+echo "" >> CMakeLists.txt
+echo "add_library(boost INTERFACE)" >> CMakeLists.txt
+echo "target_link_libraries(boost INTERFACE" >> CMakeLists.txt
+for i in "${modules[@]}"
+do
+    echo "    Boost::$i" >> CMakeLists.txt
+done
+echo ")" >> CMakeLists.txt
+echo "add_library(Boost::boost ALIAS boost)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
 
 # Commit CMake stuff
-$GIT add $CMAKE_OUT_FILE
-$GIT commit -m "Add \`CMakeLists.txt\` and \`cmake\` submodule"
+git add CMakeLists.txt
+git commit -m "Update \`CMakeLists.txt\`"
